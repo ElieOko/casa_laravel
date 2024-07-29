aec722566bd06ae43c664e70bd53db8dda9e6e94
ElieOko <elieoko100@gmail.com>
2024-07-29 15:47:26 +0100

    build:project

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+        // Route::resourceVerbs([
+        //     'create' => 'crear',
+        //     'edit' => 'editar',
+        //     'index' => 'all'
+        // ]);
+        //Model::preventSilentlyDiscardingAttributes(! $this->app->isProduction());
+    }
+}
