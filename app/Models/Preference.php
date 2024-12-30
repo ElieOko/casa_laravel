dd412b6faecf29ad53978ffb2e68c29c905757f8
ElieOko <elieoko100@gmail.com>
2024-12-30 19:17:54 +0100

    fix:model preference fillable

--- app/Models/Preference.php
+++ app/Models/Preference.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Preference extends Model
 {
     use HasFactory;
+    protected $table = "TPreferences";
+    protected $fillable = [
+        'user_id',
+        'critere_id',
+        "type_maison_id"
+    ];
 }
